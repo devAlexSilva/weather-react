674980a272961577f5dfbccdf844063279ea2d80
Alex Silva <9apsilva@gmail.com>
2022-10-15 02:34:02 -0300

    feat: add wheater data from api

--- src/components/section/Index.tsx
+++ src/components/section/Index.tsx
@@ -9,10 +9,12 @@ export const Section = ({ img, alt }: propsSection) => {
   const [wheater, setWeather] = useState({})
 
   const result = async(e: KeyboardEvent) => {
-    e.key === "Enter" && (
-    console.log(await api.get(textSearch)),
+    if(e.key !== "Enter") return;
+    
+    const wheater = await api.get(textSearch)
     setTextSearch('')
-    )
+    setWeather(wheater)
+    console.log(wheater)    
   }
 
   const dateNow = (date: Date) => {
@@ -32,15 +34,19 @@ export const Section = ({ img, alt }: propsSection) => {
           onChange={e => setTextSearch(e.target.value)}
           value={textSearch}
           ></S.Search>
+            {wheater.name &&
           <S.DataWrapper>
-              <h3>{dateNow(new Date())}</h3>
+            <h3>{wheater.name}, {wheater.sys.country}</h3>
+              <p>{dateNow(new Date())}</p>
             <S.DataTemp>
+              <p>{wheater.main.temp}°C</p>
               <S.Temp />
               <S.SvgWrapper>
                 <S.Svg />
               </S.SvgWrapper>
             </S.DataTemp>
           </S.DataWrapper>
+            }
         </S.Wrapper>
       </S.Container>
     </>
